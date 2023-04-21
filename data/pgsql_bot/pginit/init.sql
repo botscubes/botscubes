30e3755bb25edbb717d8da63381940fed2c19725
kr-ilya <kryuchkov-ilya@bk.ru>
2023-04-21 20:48:43 +0000

    upd init sql script & set service restart param

--- data/pgsql_bot/pginit/init.sql
+++ data/pgsql_bot/pginit/init.sql
@@ -3,6 +3,7 @@ CREATE TABLE IF NOT EXISTS public.bot
     id bigserial NOT NULL,
     user_id bigint,
     token character varying(50),
+    title character varying(50),
     status integer,
     PRIMARY KEY (id)
 );
