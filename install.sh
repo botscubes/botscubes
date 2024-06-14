62b278e931965d265af06ce67a2926d73faf48ae
danilbushkov <danil1357997@gmail.com>
2024-06-14 16:48:26 +0300

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 
-git clone https://github.com/botscubes/bot-worker.git ..
-git clone https://github.com/botscubes/bot-service.git ..
-git clone https://github.com/botscubes/bot-components.git ..
-git clone https://github.com/botscubes/bql.git ..
-git clone https://github.com/botscubes/user-service.git ..
-git clone https://github.com/botscubes/web-client.git ..
+
+cd ..
+
+git clone https://github.com/botscubes/bot-worker.git
+git clone https://github.com/botscubes/bot-service.git 
+git clone https://github.com/botscubes/bot-components.git 
+git clone https://github.com/botscubes/bql.git 
+git clone https://github.com/botscubes/user-service.git 
+git clone https://github.com/botscubes/web-client.git 
+
+cd -
 
 cp go.work ..
